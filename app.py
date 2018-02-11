6924ae51e2e4e43e315de89f2767460e9b615f10
Cristiano Santos <cristianossd@gmail.com>
2018-02-11 20:56:51 -0300

    Saving affiliates json

--- app.py
+++ app.py
@@ -5,16 +5,22 @@ from flask import Flask
 app = Flask(__name__)
 
 AFFILIATES_ENDPOINT = 'https://map.crossfit.com/ac'
+AFFILIATES_FILE = 'static/db/affiliates.json'
 
 @app.route('/', methods=['GET'])
 def get_affiliates():
     params = {'term': 'crossfit'}
     r = requests.get(AFFILIATES_ENDPOINT, params=params)
 
+    affiliates_num = 0
+
     if r.status_code == 200:
         affiliates_fn = lambda a: a['state'] == 'Bahia' and a['country'] == 'Brazil'
         state_affiliates = list(filter(affiliates_fn, r.json()))
 
-        print(state_affiliates)
+        with open(AFFILIATES_FILE, 'w') as f:
+            json.dump(state_affiliates, f)
 
-    return 'ok'
+        affiliates_num = len(state_affiliates)
+
+    return 'Found %d affiliates in Bahia\n' % affiliates_num
